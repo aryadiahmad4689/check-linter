9037af43cfa9ce1d1d892cd17cd0f10661ef5b9e
padaelocomunity2 <sahrulalfa45@gmail.com>
2022-06-23 01:04:49 +0800

    fix test

--- check_linter.go
+++ check_linter.go
@@ -1,14 +1,11 @@
 package check_linter
 
 import (
-	"fmt"
-
-	"github.com/aryadiahmad4689/check-linter/src/runner"
+	"github.com/fazpass/check-linter/src/runner"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func Parser() runner.Antention {
 	var data = runner.Init().Excute()
-	fmt.Println(data)
 	return *data
 }
