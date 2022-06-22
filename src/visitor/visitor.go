9a9dd358d35af59460cae5fc4298a0eef5328ea8
padaelocomunity2 <sahrulalfa45@gmail.com>
2022-06-23 01:31:15 +0800

    perbaiki response

--- src/visitor/visitor.go
+++ src/visitor/visitor.go
@@ -23,10 +23,10 @@ func (v *Visitor) Visit(n ast.Node) ast.Visitor {
 			dta, oks := fmst.Fun.(*ast.SelectorExpr)
 			if oks {
 				if dta.Sel.Name == "Print" && dta.X.(*ast.Ident).Name == "fmt" {
-					data := fmt.Sprintf("Ditemukan fmt.Print yang tidak perlu di line dan file:%s", v.Fset.Position(n.Pos()))
+					data := fmt.Sprintf("Ditemukan fmt.Print di line dan file:%s", v.Fset.Position(n.Pos()))
 					v.Data = append(v.Data, data)
 				} else if dta.Sel.Name == "Println" && dta.X.(*ast.Ident).Name == "fmt" {
-					data := fmt.Sprintf("Ditemukan fmt.Println yang tidak perlu di line dan file:%s", v.Fset.Position(n.Pos()))
+					data := fmt.Sprintf("Ditemukan fmt.Println line dan file:%s", v.Fset.Position(n.Pos()))
 					v.Data = append(v.Data, data)
 				}
 			}
@@ -34,7 +34,7 @@ func (v *Visitor) Visit(n ast.Node) ast.Visitor {
 
 	case *ast.AssignStmt:
 		if x.Tok.String() == ":=" {
-			data := fmt.Sprintf("Ditemukan fmt.Println yang tidak perlu di line dan file:%s", v.Fset.Position(n.Pos()))
+			data := fmt.Sprintf("Ditemukan := di line dan file:%s", v.Fset.Position(n.Pos()))
 			v.Data = append(v.Data, data)
 		}
 
